05dc3d40a14a43f94803d922a7efc4c9ea933192
sevenelevon <elevonseven@gmail.com>
2023-08-03 09:05:26 +0000

    deployment 14

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Hello two test project
         </a>
       </header>
     </div>
